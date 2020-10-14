8c5a4380cd8ad7f49dfe731942e157a6f9297a4f
Sophie Wigmore <swigmore@vmware.com>
2020-10-14 17:32:54 -0400

    Uses WithPullPolicy(never) instead of WithNoPull()

--- integration/passenger_test.go
+++ integration/passenger_test.go
@@ -59,7 +59,7 @@ func testPassenger(t *testing.T, context spec.G, it spec.S) {
 			var logs fmt.Stringer
 			image, logs, err = pack.WithNoColor().Build.
 				WithBuildpacks(rubyBuildpack).
-				WithNoPull().
+				WithPullPolicy("never").
 				Execute(name, source)
 			Expect(err).NotTo(HaveOccurred(), logs.String())
 
@@ -95,7 +95,7 @@ func testPassenger(t *testing.T, context spec.G, it spec.S) {
 				var logs fmt.Stringer
 				image, logs, err = pack.WithNoColor().Build.
 					WithBuildpacks(rubyBuildpack).
-					WithNoPull().
+					WithPullPolicy("never").
 					Execute(name, source)
 				Expect(err).NotTo(HaveOccurred(), logs.String())
 
