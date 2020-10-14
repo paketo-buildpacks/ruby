5980743e758ab2fb40a0f2ecf63771274af08e2d
Sophie Wigmore <swigmore@vmware.com>
2020-10-14 17:32:54 -0400

    Uses WithPullPolicy(never) instead of WithNoPull()

--- integration/rake_test.go
+++ integration/rake_test.go
@@ -59,7 +59,7 @@ func testRake(t *testing.T, context spec.G, it spec.S) {
 				var logs fmt.Stringer
 				image, logs, err = pack.WithNoColor().Build.
 					WithBuildpacks(rubyBuildpack).
-					WithNoPull().
+					WithPullPolicy("never").
 					Execute(name, source)
 				Expect(err).NotTo(HaveOccurred(), logs.String())
 
@@ -91,7 +91,7 @@ func testRake(t *testing.T, context spec.G, it spec.S) {
 				var logs fmt.Stringer
 				image, logs, err = pack.WithNoColor().Build.
 					WithBuildpacks(rubyBuildpack).
-					WithNoPull().
+					WithPullPolicy("never").
 					Execute(name, source)
 				Expect(err).NotTo(HaveOccurred(), logs.String())
 
@@ -124,7 +124,7 @@ func testRake(t *testing.T, context spec.G, it spec.S) {
 				var logs fmt.Stringer
 				image, logs, err = pack.WithNoColor().Build.
 					WithBuildpacks(rubyBuildpack).
-					WithNoPull().
+					WithPullPolicy("never").
 					Execute(name, source)
 				Expect(err).NotTo(HaveOccurred(), logs.String())
 
