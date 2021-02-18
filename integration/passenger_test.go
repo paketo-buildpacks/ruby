46f3c6e91b09c40d8e28aa526f4263f7aa3061ff
Arjun Sreedharan <asreedharan@vmware.com>
2021-02-18 22:06:43 +0000

    Adds paketo-buildpacks/image-labels to the buildpack
    Resolves: #9

--- integration/passenger_test.go
+++ integration/passenger_test.go
@@ -88,6 +88,7 @@ func testPassenger(t *testing.T, context spec.G, it spec.S) {
 			Expect(logs).To(ContainLines(ContainSubstring("Passenger Buildpack")))
 			Expect(logs).NotTo(ContainLines(ContainSubstring("Procfile Buildpack")))
 			Expect(logs).NotTo(ContainLines(ContainSubstring("Environment Variables Buildpack")))
+			Expect(logs).NotTo(ContainLines(ContainSubstring("Image Labels Buildpack")))
 		})
 
 		context("using optional utility buildpacks", func() {
@@ -95,13 +96,16 @@ func testPassenger(t *testing.T, context spec.G, it spec.S) {
 				Expect(ioutil.WriteFile(filepath.Join(source, "Procfile"), []byte("web: bundle exec passenger start --port ${PORT}"), 0644)).To(Succeed())
 			})
 
-			it("builds a working OCI image with start command from the Procfile and incorporating the utility buildpacks' effects", func() {
+			it("builds a working image that complies with utility buildpack functions", func() {
 				var err error
 				var logs fmt.Stringer
 				image, logs, err = pack.WithNoColor().Build.
-					WithEnv(map[string]string{"BPE_SOME_VARIABLE": "SOME_VALUE"}).
 					WithBuildpacks(rubyBuildpack).
 					WithPullPolicy("never").
+					WithEnv(map[string]string{
+						"BPE_SOME_VARIABLE": "SOME_VALUE",
+						"BP_IMAGE_LABELS":   "some-label=some-value",
+					}).
 					Execute(name, source)
 				Expect(err).NotTo(HaveOccurred(), logs.String())
 
@@ -132,8 +136,11 @@ func testPassenger(t *testing.T, context spec.G, it spec.S) {
 				Expect(logs).To(ContainLines(ContainSubstring("Bundle Install Buildpack")))
 				Expect(logs).To(ContainLines(ContainSubstring("Passenger Buildpack")))
 				Expect(logs).To(ContainLines(ContainSubstring("Procfile Buildpack")))
-				Expect(logs).To(ContainLines(ContainSubstring("bundle exec passenger start --port ${PORT}")))
+				Expect(logs).To(ContainLines(ContainSubstring("Image Labels Buildpack")))
 				Expect(logs).To(ContainLines(ContainSubstring("Environment Variables Buildpack")))
+				Expect(logs).To(ContainLines(ContainSubstring("bundle exec passenger start --port ${PORT}")))
+
+				Expect(image.Labels["some-label"]).To(Equal("some-value"))
 			})
 		})
 	})
